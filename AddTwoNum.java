4b1d73140db1671e89e5c56bcbf1e87b1208b0df
Gourisharma2005 <sharmagouri2005@gmail.com>
2024-11-23 07:19:17 -0800

    Add files via upload

---
+++ AddTwoNum.java
@@ -0,0 +1,17 @@
+package basicJava;
+
+import java.util.Scanner;
+public class AddTwoNum {
+
+	public static void main(String[] args) {
+		Scanner num = new Scanner(System.in);
+		System.out.print("enter first number : ");
+		int num1 = num.nextInt();
+		System.out.print("enter second number : ");
+		int num2 = num.nextInt();
+		int sum = num1 + num2;
+		System.out.print("result is :"+sum);
+
+	}
+
+}
