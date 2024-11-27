d078faeabd7aaf71452c152302161319eab0cc75
Gourisharma2005 <sharmagouri2005@gmail.com>
2024-11-27 06:50:01 -0800

    Add files via upload

---
+++ EscapeQuestion1.java
@@ -0,0 +1,14 @@
+//print the following pattern using single print statement
+//*
+//**
+//***
+//****
+
+package basicJava;
+
+public class EscapeQuestion1 {
+  public static void main(String[] args) {
+	  System.out.println("*\n**\n***\n****");
+	  System.out.println("\n****\n***\n**\n*"); //starting ka \n dono pattern mai spacing ke liye hai
+  }
+}
