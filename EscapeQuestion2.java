05381b927a08822ead857ea294d6f5bb1bd29ac5
Gourisharma2005 <sharmagouri2005@gmail.com>
2024-11-28 05:43:08 -0800

    Add files via upload

---
+++ EscapeQuestion2.java
@@ -0,0 +1,13 @@
+//print the following pattern using single print statement
+//   *
+//  **
+// ***
+//****
+
+package basicJava;
+
+public class EscapeQuestion2 {
+	 public static void main(String[] args) {
+		 System.out.println("   *\n  **\n ***\n****");
+	 }
+}
