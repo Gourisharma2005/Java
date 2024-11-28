805b15bebe8feea3818e1f4ebc0fb7d5733f87ab
Gourisharma2005 <sharmagouri2005@gmail.com>
2024-11-28 05:43:43 -0800

    Add files via upload

---
+++ EscapeSequence.java
@@ -0,0 +1,11 @@
+package basicJava;
+
+public class EscapeSequence {
+ public static void main(String[] args) {
+	 System.out.println("hello\"everyone\"");
+	 System.out.println("hello\\everyone\\");
+	 System.out.println("hello\n everyone");
+	 System.out.println("hello\t everyone");
+	 System.out.println("hello\beveryone");
+ }
+}
